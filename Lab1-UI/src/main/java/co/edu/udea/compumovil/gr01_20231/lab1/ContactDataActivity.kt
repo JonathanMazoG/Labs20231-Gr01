6af7344e1ad34f3f7dcf658224b13bd86eca7ab1
JuanLema14 <72539768+JuanLema14@users.noreply.github.com>
2023-03-05 16:34:29 -0500

    Creacion de nueva actividad

---
+++ Lab1-UI/src/main/java/co/edu/udea/compumovil/gr01_20231/lab1/ContactDataActivity.kt
@@ -0,0 +1,40 @@
+package co.edu.udea.compumovil.gr01_20231.lab1
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import co.edu.udea.compumovil.gr01_20231.lab1.databinding.ContactDataActivityBinding
+
+class ContactDataActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ContactDataActivityBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ContactDataActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+        val navController = findNavController(R.id.nav_host_fragment_content_contact_data)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        binding.fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_content_contact_data)
+        return navController.navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
+    }
+}
