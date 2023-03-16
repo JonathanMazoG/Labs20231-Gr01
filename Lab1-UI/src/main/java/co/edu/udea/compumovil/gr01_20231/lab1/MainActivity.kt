a35f772259fdc6ed38f6939b64ef97e22f0407fe
JuanLema14 <72539768+JuanLema14@users.noreply.github.com>
2023-03-15 22:25:03 -0500

    agregamos los campos para el select de grado de escolaridad y pusipos el popup de la fecha de nacimiento

--- Lab1-UI/src/main/java/co/edu/udea/compumovil/gr01_20231/lab1/MainActivity.kt
+++ Lab1-UI/src/main/java/co/edu/udea/compumovil/gr01_20231/lab1/MainActivity.kt
@@ -1,11 +1,41 @@
 package co.edu.udea.compumovil.gr01_20231.lab1
 
+import android.app.DatePickerDialog
+import android.view.View
+import android.widget.EditText
+import java.util.*
+import kotlin.collections.ArrayList
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_personal_data)
+
+        // Obtener referencia al EditText de fecha de nacimiento
+        val fechaNacimientoEditText = findViewById<EditText>(R.id.fechanacimiento_edittext)
+
+// Crear objeto DatePickerDialog.OnDateSetListener para actualizar el texto del EditText
+        val fechaNacimientoListener = DatePickerDialog.OnDateSetListener { _, year, month, dayOfMonth ->
+            // Actualizar texto del EditText con la fecha seleccionada
+            val fechaSeleccionada = "$dayOfMonth/${month + 1}/$year"
+            fechaNacimientoEditText.setText(fechaSeleccionada)
+        }
+
+// Crear objeto DatePickerDialog y configurarlo para mostrar un calendario
+        val calendar = Calendar.getInstance()
+        val anioActual = calendar.get(Calendar.YEAR)
+        val mesActual = calendar.get(Calendar.MONTH)
+        val diaActual = calendar.get(Calendar.DAY_OF_MONTH)
+        val dialog = DatePickerDialog(this, fechaNacimientoListener, anioActual, mesActual, diaActual)
+        dialog.datePicker.maxDate = Date().time // Limitar la fecha máxima al día de hoy
+
+// Asignar evento onClick al EditText para mostrar el DatePickerDialog
+        fechaNacimientoEditText.setOnClickListener {
+            dialog.show()
+        }
+
     }
 }
